227b865443186c6c3c4491e8e84a3dadc54f11a0
j.cevallosmoreno <jesusfcevallos@gmail.com>
2023-02-08 22:56:23 +0100

    new compiled version

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,9 @@
-/*
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-*/
-module.exports = {
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
       ? '/zenyth/'
       : '/'
-}
+})
+
 
