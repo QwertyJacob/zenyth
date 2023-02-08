568c7ac89b9cd7b2b31066a4037b5790803fa823
Elia Pirrello <56640501+EliaPirrello@users.noreply.github.com>
2023-02-08 12:37:20 +0100

    Fixed Deploy

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,11 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 
+/*
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
       ? '/zenyth/'
       : '/'
 }
+
+ */
