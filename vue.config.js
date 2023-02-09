117bf5382563de8945e47251006be322784d34e8
j.cevallosmoreno <jesusfcevallos@gmail.com>
2023-02-09 21:10:00 +0100

    site on docs

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+/*
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -5,5 +6,17 @@ module.exports = defineConfig({
       ? '/zenyth/'
       : '/'
 })
+*/
 
+const path = require("path");
+
+
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/zenyth/'
+      : '/',
+  outputDir: path.resolve(__dirname, "docs"),
+
+}
 
